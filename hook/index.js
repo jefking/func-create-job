c5c58321a19ddacbd9d5b1422e5d2c4d4e6e5f91
root <root@DESKTOP-74N9G6D.localdomain>
2018-09-26 11:03:22 -0700

    azure batch

--- hook/index.js
+++ hook/index.js
@@ -1,7 +1,15 @@
-module.exports = function (context, req) {
+const batch = require('azure-batch');
+let accountName = process.env.BatchAccountName;
+let accountKey = process.env.BatchAccountKey;
+let accountUrl = process.env.BatchAccountUrl;
+
+module.exports = function(context, req) {
     let error = null;
 
     if (typeof req.body != 'undefined' && typeof req.body == 'object') {
+        let credentials = new batch.SharedKeyCredentials(accountName, accountKey);
+        let batch_client = new batch.ServiceClient(credentials, accountUrl);
+
         let payload = req.body;
         context.bindings.out = payload;
         context.log(payload);
