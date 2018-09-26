2edbc88d5237b8ba7997ac1a157fb4a786289c74
root <root@DESKTOP-74N9G6D.localdomain>
2018-09-26 11:41:56 -0700

    batch

--- hook/index.js
+++ hook/index.js
@@ -2,6 +2,7 @@ const batch = require('azure-batch');
 let accountName = process.env.BatchAccountName;
 let accountKey = process.env.BatchAccountKey;
 let accountUrl = process.env.BatchAccountUrl;
+let poolid = process.env.BatchPool;
 
 module.exports = function(context, req) {
     let error = null;
@@ -10,9 +11,25 @@ module.exports = function(context, req) {
         let credentials = new batch.SharedKeyCredentials(accountName, accountKey);
         let batch_client = new batch.ServiceClient(credentials, accountUrl);
 
-        let payload = req.body;
-        context.bindings.out = payload;
+        //LOG
         context.log(payload);
+
+        //DATA
+        let payload = req.body;
+
+        // Setting up Batch pool configuration
+        var pool_config = { poolId: poolid }
+        // Setting up Job configuration along with preparation task
+        var jobId = "processimagejob"
+        var job_config = { id: jobId, displayName: "process file", poolInfo: pool_config }
+        // Adding Azure batch job to the pool
+        var job = batch_client.job.add(job_config, function(error, result) {
+            if (error != null) {
+                console.log("Error submitting job : " + error.response);
+            }
+        });
+
+        //RESPONSE
         context.res.sendStatus(200);
     }
     else {
