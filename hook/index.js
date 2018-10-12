4407d01375adc9543962abcf4252a2cc45514660
Jef King <jefkin@microsoft.com>
2018-10-12 14:37:21 -0700

    logging

--- hook/index.js
+++ hook/index.js
@@ -8,21 +8,24 @@ module.exports = function (context) {
     let error = null;
 
     let blob = context.bindings.myBlob;
+    context.log(blob);
 
     let credentials = new batch.SharedKeyCredentials(accountName, accountKey);
     let batch_client = new batch.ServiceClient(credentials, accountUrl);
 
-    //Log
-    context.log(blob);
+    let poolid = guid();
+    context.log(poolid);
 
-    // // Setting up Batch pool configuration
-    // var pool_config = { poolId: poolid }
-    
-    // // Setting up Job configuration along with preparation task
-    // var jobId = "processimagejob"
-    // var job_config = { id: jobId, displayName: "process file: " + blob, poolInfo: pool_config }
-    
-    // // Adding Azure batch job to the pool
+    // Setting up Batch pool configuration
+    var pool_config = { poolId: poolid }
+
+    // Setting up Job configuration along with preparation task
+    var jobId = "processimagejob"
+    context.log(jobId);
+    var job_config = { id: jobId, displayName: "process file: " + blob, poolInfo: pool_config }
+    context.log(job_config);
+
+    // Adding Azure batch job to the pool
     // var job = batch_client.job.add(job_config, function (error, result) {
     //     if (error != null) {
     //         console.log("Error submitting job : " + error.response);
@@ -31,3 +34,12 @@ module.exports = function (context) {
 
     context.done(error);
 };
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
